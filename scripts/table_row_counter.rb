72c57b46653b3d1d053500f2fbde23714de35250
Alberto Espinoza <aespinoz@quoininc.com>
2023-05-12 10:41:31 -0600

    Adding solr queries in verification script

--- scripts/table_row_counter.rb
+++ scripts/table_row_counter.rb
@@ -5,3 +5,10 @@ ActiveRecord::Base.connection.tables.each do |table|
   count = ActiveRecord::Base.connection.select_value("SELECT COUNT(*) FROM #{quoted_table_name}")
   puts "table= #{table}, count= #{count}"
 end
+
+puts "Total records with status open, record_state true and created_at from 01/01/2020 and Today"
+puts Child.search { with(:status, 'open'); with(:record_state, true); with(:created_at, DateTime.new(2020, 01, 01).beginning_of_day..DateTime.now.end_of_day) }.total
+
+
+puts "Total records with status open, record_state false and created_at from 01/01/2020 and Today"
+puts Child.search { with(:status, 'open'); with(:record_state, false); with(:created_at, DateTime.new(2020, 01, 01).beginning_of_day..DateTime.now.end_of_day) }.total
